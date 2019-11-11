56dc6bb1dfca54f811103530a158b7888c7a9fcc
gitkiselev <482003@mail.ru>
2019-11-11 15:38:12 +0300

    HW08 basic done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
-import styled from "styled-components";
+//import styled from "styled-components";
+import './app.css';
 
 
-const App = () => {
-    return (
-        <> 
-            
-            <Container>
-                <Header />
+export default class App extends Component {
+    state = {
+        visible: true
+    }
+    
+    toggleVisibility =() => {
+        this.setState(({visible}) => ({
+            visible: !visible
+        }));
+    }
+    render() {
+        return (
+            <> 
                 
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+                <Container>
+                    <Header />
+                    
+                </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                        <button className="btn-toggle" onClick={this.toggleVisibility}>
+                            {this.state.visible === true ? 'Hide' : 'Show'}
+                        </button>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {this.state.visible ? <RandomChar/> : null}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+    
 };
 
-export default App;
--- src/services/gotService.js
+++ src/services/gotService.js
@@ -9,47 +9,108 @@ export default class GotService {
         }
         return await res.json();
     }
-    getAllCharacters() {
-        return this.getResource('/characters?page=5&pageSize=10');
+    async getAllCharacters() {
+        const res = await this.getResource('/characters?page=5&pageSize=10');
+        return res.map(this._transformCharacter)
     }
-    getCharacter(id) {
-        return this.getResource(`/characters/${id}`);
-    }
-
-    getAllBooks() {
-        return this.getResource('/books?page=1&pageSize=10');
-    }
-    getBook(id) {
-        return this.getResource(`/books/${id}`);
+    async getCharacter(id) {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character)
     }
 
-    getAllHouses() {
-        return this.getResource('/houses?page=5&pageSize=10');
+
+    async getAllBooks() {
+        const res = await this.getResource('/books');
+        return res.map(this._transformBook)
     }
-    getHouse(id) {
-        return this.getResource(`/houses/${id}`);
+    async getBook(id) {
+        const book = await this.getResource(`/books/${id}`);
+        return this._transformBook(book)
+    }
+
+
+    async getAllHouses() {
+        const res = await this.getResource('/houses');
+        return res.map(this._transformHouse)
+    }
+    async getHouse(id) {
+        const house = await this.getResource(`/houses/${id}`);
+        return this._transformBook(house)
+    }
+
+
+
+
+
+
+
+
+
+
+    // getAllBooks() {
+    //     return this.getResource('/books?page=1&pageSize=10');
+    // }
+    // getBook(id) {
+    //     return this.getResource(`/books/${id}`);
+    // }
+
+    // getAllHouses() {
+    //     return this.getResource('/houses?page=5&pageSize=10');
+    // }
+    // getHouse(id) {
+    //     return this.getResource(`/houses/${id}`);
+    // }
+
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
+    }
+
+    _transformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publiser: book.publiser,
+            released: book.released
+        }
     }
 }
 
 const got = new GotService();
 
-got.getAllCharacters()
-    .then(res => {
-        res.forEach(item => console.log(item.name));
-    });
-got.getCharacter(130)
-    .then(res => console.log(res));
+// got.getAllCharacters()
+//     .then(res => {
+//         res.forEach(item => console.log(item.name));
+//     });
+// got.getCharacter(130)
+//     .then(res => console.log(res));
 
-got.getAllBooks()
-.then(res => {
-    res.forEach(item => console.log(item.name));
-});
-got.getBook(2)
-    .then(res => console.log(res));
+// got.getAllBooks()
+// .then(res => {
+//     res.forEach(item => console.log(item.name));
+// });
+// got.getBook(2)
+//     .then(res => console.log(res));
 
-    got.getAllHouses()
-    .then(res => {
-        res.forEach(item => console.log(item.name));
-    });
-got.getHouse(130)
-    .then(res => console.log(res));
+// got.getAllHouses()
+// .then(res => {
+//     res.forEach(item => console.log(item.name));
+// });
+// got.getHouse(130)
+//     .then(res => console.log(res));
